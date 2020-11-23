4344eefdecabbd1b335a0715a442958d3d3cb17f
kayambi <you@example.com>
2020-11-22 21:45:58 -0800

    updated temp,calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add (x,y)
+     x + y 
+end 
+def subtract(x,y)
+    x - y 
+end
+
+arr = []
+def sum(arr)
+    arr.reduce(0, :+)
+end
+
+def multiply (*numbers)
+    numbers.reduce(:*)
+end
+
+def power(x,y)
+     x**y
+end
